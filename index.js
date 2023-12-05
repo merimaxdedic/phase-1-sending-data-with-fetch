39175a428769a831a2f116f1a9f9a578cb76ad24
Merima Dedic <merimaxdedic@gmail.com>
2023-12-04 19:44:30 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  const body = JSON.stringify(userData);
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+
+  return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: headers,
+    body: body
+  })
+    .then(response => response.json())
+    .then(data => {
+      const id = data.id;
+      const idElement = document.createElement('p');
+      idElement.textContent = `New ID: ${id}`;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      const errorElement = document.createElement('p');
+      errorElement.textContent = `Error: ${error.message}`;
+      document.body.appendChild(errorElement);
+    });
+}
